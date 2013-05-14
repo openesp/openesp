1d460b009b279fcb3049be702835ead8d41b6278
Jan Høydahl <jan.git@cominvent.com>
2013-05-14 23:52:06 +0200

    Quote options

--- initial/bin/run.cmd
+++ initial/bin/run.cmd
@@ -6,11 +6,11 @@ REM *********************************************************
 
 echo "Starting OpenESP"
 
-SET OPENESP_HOME=%~dp0\..
+SET OPENESP_HOME=%~dp0..
 SET SOLR_HOME=%OPENESP_HOME%\conf\solr
 SET SOLR_DATA_DIR=%OPENESP_HOME%/data/solr
-SET CATALINA_OPTS=-Xms512m -Xmx1024m -Dsolr.solr.home=%SOLR_HOME% -Dopenesp.home=%OPENESP_HOME% -Dsolr.data.dir=%SOLR_DATA_DIR% -Dorg.apache.manifoldcf.configfile=%OPENESP_HOME%\conf\mcf\properties.xml -Dlog4j.configuration=file:///%OPENESP_HOME%\conf\solr\log4j.properties -Dlog4j.debug=true -Dsolr.log.dir=%OPENESP_HOME%\logs\solr
+SET CATALINA_OPTS=-Xms512m -Xmx1024m -Dsolr.solr.home="%SOLR_HOME%" -Dopenesp.home="%OPENESP_HOME%" -Dsolr.data.dir="%SOLR_DATA_DIR%" -Dorg.apache.manifoldcf.configfile="%OPENESP_HOME%\conf\mcf\properties.xml" -Dlog4j.configuration="file:///%OPENESP_HOME%\conf\solr\log4j.properties" -Dlog4j.debug=true -Dsolr.log.dir="%OPENESP_HOME%\logs\solr"
 echo CATALINA_OPTS is %CATALINA_OPTS%
 
-cd %OPENESP_HOME%\tomcat\bin
+cd "%OPENESP_HOME%\tomcat\bin"
 catalina.bat run
