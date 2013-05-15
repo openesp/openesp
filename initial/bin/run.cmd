36c2e3baefee998fd245f9c9ef41c37a14564273
Jan Høydahl <jan.git@cominvent.com>
2013-05-15 17:46:25 +0200

    Fixes #42

--- initial/bin/run.cmd
+++ initial/bin/run.cmd
@@ -4,8 +4,19 @@ REM * This is a DRAFT start script for development use only.
 REM * For production use, please start OpenESP as a service
 REM *********************************************************
 
-echo "Starting OpenESP"
+echo "Detecting JAVA_HOME"
+if "%JAVA_HOME%"=="" call:FIND_JAVA_HOME
+echo "Java home: %JAVA_HOME%"
+rem Start your java application or perform other actions here
+goto:END
 
+:FIND_JAVA_HOME
+FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\JavaSoft\Java Runtime Environment" /v CurrentVersion') DO set CurVer=%%B
+FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\Software\JavaSoft\Java Runtime Environment\%CurVer%" /v JavaHome') DO set JAVA_HOME=%%B
+goto:EOF
+:END
+
+echo "Starting OpenESP"
 SET OPENESP_HOME=%~dp0..
 SET SOLR_HOME=%OPENESP_HOME%\conf\solr
 SET SOLR_DATA_DIR=%OPENESP_HOME%/data/solr
